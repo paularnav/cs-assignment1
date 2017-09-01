60c6764178ec1b4e7037feeea1d4e19e9e6028da
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-01 17:57:01 +0530

    Create a1q1.cpp

---
+++ a1q1.cpp
@@ -0,0 +1,28 @@
+# cs-assignment1
+//question 1
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a;
+ float b;
+ char c;
+ double d;
+ bool e;
+ cout<<"Enter the integer : ";
+ cin>>a;
+ cout<<"Enter the float : ";
+ cin>>b;
+ cout<<"Enter the char : ";
+ cin>>c;
+ cout<<"Enter the double : ";
+ cin>>d;
+ cout<<"Enter the bool : ";
+ cin>>e;
+ cout<<"Integer : "<<a;
+ cout<<"Float : "<<b;
+ cout<<"Char : "<<c;
+ cout<<"double : "<<d;
+ cout<<"bool : "<<e;
+ return 0;
+} 
