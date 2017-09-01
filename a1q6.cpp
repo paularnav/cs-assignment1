f55ae8f9b076a55031a86c6017dbedf8efe0f3cd
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-01 18:14:45 +0530

    Create a1q6.cpp

---
+++ a1q6.cpp
@@ -0,0 +1,15 @@
+# cs-assignment1
+//question 6
+#include<iostream>
+using namespace std;
+main()
+{
+float a,b,c;
+cout<<"enter radius";
+cin >> a;
+b = 3.14*(a*a);
+cout << "area =" <<b;
+c = 2*3.14*a;
+cout<< "circumference = "<<c;
+return 0;
+}
