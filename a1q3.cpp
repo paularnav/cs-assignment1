adfc1779e4020c557cb3d0191bd9493c2a1778ab
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-01 18:05:58 +0530

    Create a1q3.cpp

---
+++ a1q3.cpp
@@ -0,0 +1,19 @@
+# cs-assignment1
+question 3
+#include<iostream>
+using namespace std;
+main()
+{
+int a,b,c;
+cout<<"enter two numbers";
+cin >> a >> b;
+c = a*b;
+cout << "product =" <<c;
+c= a+b;
+cout<<"sum = "<<c;
+c=a-b;
+cout<<"difference = "<<c;
+c=a/b;
+cout<<"divison = "<<c;
+return 0;
+}
