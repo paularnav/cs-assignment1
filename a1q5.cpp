dc6389c3566b3ddcac9633e2aa962b4e89eb70c8
paularnav <31165622+paularnav@users.noreply.github.com>
2017-09-01 18:08:03 +0530

    Create a1q5.cpp

---
+++ a1q5.cpp
@@ -0,0 +1,13 @@
+# cs-assignment1
+//question 5
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"enter length and breadth for rectangle";
+cin >> a >> b;
+c = a*b;
+cout << "area =" <<c;
+return 0;
+}
